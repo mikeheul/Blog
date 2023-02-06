09aecea01ec861b6dec27d62db11b346e5cf4ea8
Micka <mickael.murmann@gmail.com>
2023-02-06 14:17:03 +0100

    generate articles JSON

--- js/script.js
+++ js/script.js
@@ -1,5 +1,80 @@
-window.addEventListener("DOMContentLoaded", () =>{
-    
+let dataArticles = {
+    1: {
+        "category": "photography",
+        "img": "blog1",
+        "title": "Mon test",
+        "user": "quentin",
+        "date": "February 2nd, 2023",
+        "likes": 111,
+        "text": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed dotempor incididunt ut. labore et dolore magna aliqua. Ut enim ad minim veniam",
+    },
+}
+
+const blogs = document.querySelector(".blog .row")
+
+for(article in dataArticles) {
+    let blog_article = document.createElement("div")
+    blog_article.classList.add("blog-article")
+    blog_article.classList.add("col-4")
+    blog_article.classList.add(dataArticles[article].category)
+    blog_article.innerHTML = 
+        "<div class='blog-img'>" + 
+        "<img src='img/" + dataArticles[article].img + ".jpg' alt='" + dataArticles[article].img + "'>" +
+        "<div class='overlay-img'></div>" +
+        "<div class='title'>" +
+            "<h3>" + dataArticles[article].title + "</h3>" +
+            "<p>" +
+                "<span><i class='fa-regular fa-user'></i> By " + dataArticles[article].user + "</span>" +
+                "<span><i class='fa-regular fa-calendar'></i> " + dataArticles[article].date + "</span>" +
+                "<span><i class='fa-regular fa-heart'></i> " + dataArticles[article].likes + "</span>" +
+            "</p>" +
+        "</div>" +
+        "<div class='category " + dataArticles[article].category + "'>" +
+            "<span>" + dataArticles[article].category + "</span>" +
+        "</div>" +
+    "</div>" +
+    "<div class='blog-text'>" +
+        "<p class='text-content'>" +
+            dataArticles[article].text
+        "</p>" +
+        "<p class='read'>" +
+            "<a href=''>Read More <i class='fa-solid fa-arrow-right'></i></a>" +
+        "</p>" +
+    "</div>"
+
+    blogs.appendChild(blog_article)
+}
+
+/* <div class="blog-article col-4 photography">
+    <div class="blog-img">
+        <img src="img/blog1.jpg" alt="blog1">
+        <div class="overlay-img"></div>
+        <div class="title">
+            <h3>China Trip</h3>
+            <p>
+                <span><i class="fa-regular fa-user"></i> By micka </span>
+                <span><i class="fa-regular fa-calendar"></i> January 1st, 2023</span>
+                <span><i class="fa-regular fa-heart"></i> 123</span>
+            </p>
+        </div>
+        <div class="category photography">
+            <span>Photography</span>
+        </div>
+    </div>
+    <div class="blog-text">
+        <p class="text-content">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed dotempor incididunt ut. labore et dolore magna aliqua. Ut enim ad minim veniam
+        </p>
+        <p class="read">
+            <a href="">Read More <i class="fa-solid fa-arrow-right"></i></a>
+        </p>
+    </div>
+</div> */
+
+
+
+window.addEventListener("DOMContentLoaded", () => {
+
     const burger = document.querySelector(".burger");
     const menu_responsive = document.querySelector(".menu-responsive");
     const filters = document.querySelector(".filters");
@@ -16,7 +91,7 @@ window.addEventListener("DOMContentLoaded", () =>{
     const btnNature = filters.querySelector(".nature");
     const btnFood = filters.querySelector(".food");
     const btnPhotography = filters.querySelector(".photography");
-    
+
     btnAll.innerHTML += " (" + nbAll + ")"
     btnLifestyle.innerHTML += " (" + nbLifestyle + ")"
     btnNature.innerHTML += " (" + nbNature + ")"
@@ -25,7 +100,7 @@ window.addEventListener("DOMContentLoaded", () =>{
 
     burger.addEventListener("click", (e) => {
         e.preventDefault();
-        if(menu_responsive.classList.contains("visible")) {
+        if (menu_responsive.classList.contains("visible")) {
             burger.querySelector("a i").classList.add("fa-bars")
             burger.querySelector("a i").classList.remove("fa-close")
             menu_responsive.classList.remove("visible");
@@ -38,16 +113,16 @@ window.addEventListener("DOMContentLoaded", () =>{
 
     filters.addEventListener("click", (e) => {
         e.preventDefault();
-        if(e.target.classList.contains("filter-button")) {
+        if (e.target.classList.contains("filter-button")) {
             filters.querySelector(".active").classList.remove("active");
             e.target.classList.add("active");
             const filterValue = e.target.getAttribute("data-filter");
-            articles.forEach((item) =>{
-                if(item.classList.contains(filterValue) || filterValue === 'all'){
+            articles.forEach((item) => {
+                if (item.classList.contains(filterValue) || filterValue === 'all') {
                     item.classList.remove("hide");
                     item.classList.add("show");
                 }
-                else{
+                else {
                     item.classList.remove("show");
                     item.classList.add("hide");
                 }
